df21e2454b3ea565289323d42a62bf896bb5f8cf
Adrian Fernandez <adrianfernandezalv@gmail.com>
2024-08-04 22:33:33 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder().setTitle("AnglrBrain").setDescription("API Documentation").setVersion("1.0").build();
-
   const document = SwaggerModule.createDocument(app, options);
-
   SwaggerModule.setup("api", app, document);
 
   await app.listen(3000);
