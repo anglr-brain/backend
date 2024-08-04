3ead2ad6c59f7bdf26421556559c4cdbdfdd5c89
Adrian Fernandez <adrianfernandezalv@gmail.com>
2024-08-04 22:29:31 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder().setTitle("AnglrBrain").setDescription("API Documentation").setVersion("1.0").build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
