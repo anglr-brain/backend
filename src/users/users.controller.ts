485ab33ef88dbe429c03c22cb35564aaa0f5c5d7
Adrian Fernandez <adrianfernandezalv@gmail.com>
2024-08-05 13:53:49 +0200

    add endpoint info

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,20 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { User } from "./entities/user.entity";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @ApiTags("users")
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: "Get all users" })
   @Get()
   public getUsers(): Promise<User[]> {
     return this.usersService.getUsers();
   }
 
+  @ApiOperation({ summary: "Get user by ID" })
   @Get(":id")
   public getUser(@Param("id") id: string): Promise<User> {
     return this.usersService.getUser(id);
